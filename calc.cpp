3d845389f0cdc1cda2ec52abcd34d0961769a97a
Kov <maksim.isakov.97@mail.ru>
2021-03-11 01:21:28 +0300

    sdelal funk +

--- calc.cpp
+++ calc.cpp
@@ -1,33 +1,45 @@
-+//файл calc.cpp 
-+#include "stdafx.h" 
-+#include <iostream> 
-+using namespace std;
-+
-+int main() 
-+{	
-+	setlocale(LC_ALL, "rus");
-+
-+	int c;
-+	double x, y;
-+	
-+	cout << "Введите X" << endl;
-+	cin >> x;
-+	cout << "Введите Y" << endl;
-+	cin >> y;
-+	cout << "1:Сумма 2:Вычитание 3:Умножение 4:Деление" << endl;
-+	cin >> c;
-+	switch (c)
-+	{
-+	case 1:
-+	case 2:
-+	case 3:
-+	case 4:
-+		cout << "Выбранная функция в разработке" << endl;
-+		break;
-+	default:
-+		cout << "Несуществующая функция" << endl;
-+		break;
-+	}
-+	system("pause");
-+	return 0;
-+}
+﻿//файл calc.cpp 
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int c;
+	double x, y;
+
+	cout << "Введите X" << endl;
+	cin >> x;
+	cout << "Введите Y" << endl;
+	cin >> y;
+	cout << "1:Сумма 2:Вычитание 3:Умножение 4:Деление" << endl;
+	cin >> c;
+	switch (c)
+	{
+	case 1:
+		cout << MySum(x, y) << endl;
+		break;
+	case 2:
+		cout << MySub(x, y) << endl;
+		break;
+	case 3:
+		cout << MyMul(x, y) << endl;
+		break;
+	case 4:
+		cout << "Выбранная функция в разработке" << endl;
+		cout << MyDiv(x, y) << endl;
+		break;
+	default:
+		cout << "Несуществующая функция" << endl;
+
+		cout << "Выбранная функция в разработке" << endl;
+		break;
+	default:
+		cout << "Несуществующая функция" << endl;
+		break;
+	}
+	system("pause");
+	return 0;
+}
